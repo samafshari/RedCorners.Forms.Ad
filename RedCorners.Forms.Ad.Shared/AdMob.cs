104181f0604eec4f9c20b1481020761a2bfd5ab4
s_a@live.com (R) <s_a@live.com>
2020-11-05 10:33:43 +0100

    Initial commit

---
+++ RedCorners.Forms.Ad.Shared/AdMob.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+#if __IOS__
+using Google.MobileAds;
+using Xamarin.Forms.Platform.iOS;
+#endif
+
+#if __ANDROID__
+using Android.Gms.Ads;
+#endif
+
+namespace RedCorners.Forms.Ad
+{
+    public class AdMob : IAd
+    {
+        public enum Genders : long
+        {
+            Unknown,
+            Male,
+            Female
+        }
+
+        public bool IsEnabled { get; set; } = true;
+        public string BannerId { get; set; }
+        public string[] TestDevices { get; set; }
+        public bool IsSimulatorTestDevice { get; set; } = true;
+        public string[] Keywords { get; set; }
+        public DateTime? Birthday { get; set; }
+        public Genders? Gender { get; set; }
+        public (float Latitude, float Longitude)? Location { get; set; }
+
+#if __ANDROID__
+        class MyAdListener : AdListener
+        {
+            public override void OnAdClosed()
+            {
+                AdmobPlugin.Instance.LoadAd();
+            }
+        }
+#endif
+
+#if __IOS__
+        Interstitial adInterstitial;
+#endif
+#if __ANDROID__
+        InterstitialAd adInterstitial;
+        AdRequest request;
+#endif
+
+        public AdMob()
+        {
+        }
+
+#if __IOS__
+        public void CreateAndRequestInterstitial(string interstitialId)
+        {
+            if (!IsEnabled)
+                return;
+
+            adInterstitial = new Interstitial(interstitialId);
+            adInterstitial.ScreenDismissed += (sender, e) =>
+            {
+                // Interstitial is a one time use object. That means once an interstitial is shown, HasBeenUsed 
+                // returns true and the interstitial can't be used to load another ad. 
+                // To request another interstitial, you'll need to create a new Interstitial object.
+                adInterstitial.Dispose();
+                adInterstitial = null;
+                CreateAndRequestInterstitial(interstitialId);
+            };
+
+            // Requests test ads on devices you specify. Your test device ID is printed to the console when
+            // an ad request is made. GADBannerView automatically returns test ads when running on a
+            // simulator. After you get your device ID, add it here
+
+            var request = GetDefaultRequest();
+            adInterstitial.LoadRequest(request);
+        }
+
+        public Request GetDefaultRequest()
+        {
+            var request = Request.GetDefaultRequest();
+
+            var testDevices = new List<string>();
+            if (IsSimulatorTestDevice)
+                testDevices.Add(Request.SimulatorId.ToString());
+
+            if (TestDevices != null)
+                testDevices.AddRange(TestDevices);
+
+            MobileAds.SharedInstance.RequestConfiguration.TestDeviceIdentifiers = testDevices.ToArray();
+
+            if (Keywords != null)
+                request.Keywords = Keywords;
+
+            if (Birthday != null)
+                request.Birthday = Birthday.Value.ToNSDate();
+
+            if (Gender != null)
+                request.Gender = (Google.MobileAds.Gender)Gender.Value;
+
+            if (Location != null)
+                request.SetLocation(new nfloat(Location.Value.Latitude), new nfloat(Location.Value.Longitude), 100);
+
+            return request;
+        }
+
+#else
+        public void CreateAndRequestInterstitial()
+        {
+            adInterstitial = new InterstitialAd(Forms.Context);
+            adInterstitial.AdUnitId = Vars.InterstitialId;
+            adInterstitial.AdListener = new MyAdListener();
+            LoadAd();
+        }
+
+        public void LoadAd()
+        {
+            if (!IsAdEnabled)
+                return;
+
+            request = new AdRequest.Builder()
+                .AddKeyword("luxembourg")
+#if DEBUG
+                .AddTestDevice("ED7EA56976FC3A16579281D56DB108F8")
+#endif
+                .Build();
+            adInterstitial.LoadAd(request);
+        }
+#endif
+
+        public void ShowInterstitial(Page page)
+        {
+            if (!IsEnabled)
+                return;
+
+#if __IOS__
+            var renderer = Platform.GetRenderer(page);
+            if (renderer == null)
+            {
+                renderer = Platform.CreateRenderer(page);
+                Platform.SetRenderer(page, renderer);
+            }
+            var viewController = renderer.ViewController;
+
+            if (adInterstitial.IsReady)
+                adInterstitial.Present(viewController);
+#endif
+#if __ANDROID__
+            if (adInterstitial.IsLoaded)
+            {
+                adInterstitial.Show();
+                LogSystem.Instance.TrackEvent("ShowAd");
+            }
+            else if (adInterstitial.IsLoading)
+                LogSystem.Instance.TrackEvent("LoadingAd");
+            else LogSystem.Instance.TrackEvent("IgnoreAd");
+#endif
+        }
+    }
+}
---
+++ RedCorners.Forms.Ad.Shared/AdMobBannerView.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+#if __ANDROID__
+using Xamarin.Forms.Platform.Android;
+#else
+using Xamarin.Forms.Platform.iOS;
+using Google.MobileAds;
+using CoreGraphics;
+using UIKit;
+#endif
+
+[assembly: ExportRenderer(typeof(RedCorners.Forms.Ad.AdMobBannerView), typeof(RedCorners.Forms.Ad.Renderers.AdMobBannerViewRenderer))]
+namespace RedCorners.Forms.Ad
+{
+    public enum AdMobBannerSizes
+    {
+        LargeBanner,
+        MediumRectangle
+    }
+
+    public class AdMobBannerView : View
+    {
+        public static readonly BindableProperty BannerSizeProperty = BindableProperty.Create(
+           propertyName: nameof(BannerSize),
+           returnType: typeof(AdMobBannerSizes),
+           declaringType: typeof(AdMobBannerView),
+           defaultValue: AdMobBannerSizes.LargeBanner);
+
+        public AdMobBannerSizes BannerSize
+        {
+            get => (AdMobBannerSizes)GetValue(BannerSizeProperty);
+            set => SetValue(BannerSizeProperty, value);
+        }
+        
+        public static readonly BindableProperty UnitIdProperty = BindableProperty.Create(
+           propertyName: nameof(UnitId),
+           returnType: typeof(string),
+           declaringType: typeof(AdMobBannerView));
+
+        public string UnitId
+        {
+            get => (string)GetValue(UnitIdProperty);
+            set => SetValue(UnitIdProperty, value);
+        }
+        
+        public static readonly BindableProperty AdMobProperty = BindableProperty.Create(
+           propertyName: nameof(AdMob),
+           returnType: typeof(AdMob),
+           declaringType: typeof(AdMobBannerView),
+           defaultValue: new AdMob());
+
+        public AdMob AdMob
+        {
+            get => (AdMob)GetValue(AdMobProperty);
+            set => SetValue(AdMobProperty, value);
+        }
+    }
+
+    namespace Renderers
+    {
+        public class AdMobBannerViewRenderer : ViewRenderer
+        {
+            public AdMobBannerViewRenderer() { }
+
+#if __ANDROID__
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null)
+            {
+                var adsbanner = (BannerAd)Element;
+                //var adview = new Android.Gms.Ads.NativeExpressAdView(Context);
+                //adview.AdUnitId = Constants.NativeAdId;
+                //adview.AdSize = AdSize.SmartBanner;
+                var adview = new AdView(Context);
+        if (adsbanner.AdSize == BannerAdSizes.LargeBanner)
+                adview.AdSize = AdSize.LargeBanner;
+        else if (adsbanner.AdSize == BannerAdSizes.MediumRectangle)
+        adview.AdSize = AdSize.MediumRectangle;
+                adview.AdUnitId = Vars.BannerId;
+
+                base.SetNativeControl(adview);
+
+                var requestbuilder = new AdRequest.Builder()
+#if DEBUG
+                    .AddTestDevice("ED7EA56976FC3A16579281D56DB108F8")
+                    .AddTestDevice("8EBFBD0CEA9400AE134EB85743BD37E2")
+#endif
+                    .AddKeyword("luxembourg");
+                adview.LoadAd(requestbuilder.Build());
+                adview.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+#elif __IOS__
+            BannerView bannerView;
+            bool viewOnScreen = false;
+
+            protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+            {
+                base.OnElementChanged(e);
+                if (e.NewElement == null) return;
+                if (e.OldElement == null)
+                {
+                    UIViewController viewCtrl = null;
+
+                    foreach (UIWindow v in UIApplication.SharedApplication.Windows)
+                    {
+                        if (v.RootViewController != null)
+                        {
+                            viewCtrl = v.RootViewController;
+                        }
+                    }
+
+                    var view = (AdMobBannerView)Element;
+                    view.PropertyChanged += View_PropertyChanged;
+                    var size = AdSizeCons.LargeBanner;
+                    if (view.BannerSize == AdMobBannerSizes.MediumRectangle)
+                        size = AdSizeCons.MediumRectangle;
+                    bannerView = new BannerView(size: AdSizeCons.LargeBanner, origin: new CGPoint(0, 0))
+                    {
+                        AdUnitId = view.UnitId,
+                        RootViewController = viewCtrl
+                    };
+
+                    bannerView.AdReceived += (sender, args) =>
+                    {
+                        if (!viewOnScreen)
+                            this.AddSubview(bannerView);
+                        viewOnScreen = true;
+                    };
+                    bannerView.ReceiveAdFailed += (sender, args) =>
+                    {
+                    };
+                    if (!string.IsNullOrWhiteSpace(bannerView.AdUnitId))
+                        LoadRequest();
+                    base.SetNativeControl(bannerView);
+                }
+            }
+
+            private void View_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+            {
+                var view = Element as AdMobBannerView;
+                if (e.PropertyName == nameof(AdMobBannerView.UnitId))
+                {
+                    bannerView.AdUnitId = view.UnitId;
+                    LoadRequest();
+                }
+            }
+
+            public override void MovedToWindow()
+            {
+                base.MovedToWindow();
+                if (bannerView == null) return;
+                if (bannerView.RootViewController != null) return;
+                bannerView.RootViewController = UIApplication.SharedApplication.Windows[0].RootViewController;
+            }
+
+            void LoadRequest()
+            {
+                var view = Element as AdMobBannerView;
+                if (view == null) return;
+                var adMob = view.AdMob;
+                if (adMob == null) return;
+
+                var request = adMob.GetDefaultRequest(); 
+                bannerView.LoadRequest(request);
+            }
+#endif
+        }
+    }
+}
